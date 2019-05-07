0e129d308d8769b5255e63b0145819b7ca629de4
unknown <snopezi@gmail.com>
2019-05-07 13:14:54 +0700

    set cookie

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     search = request.args.get('search')
     video = request.args.get('video')
-    if not search:
-        return render_template('index.html')
-    if not video:
-        return 'hasil search adalah ' + search
-        
-    return 'hasil search adalah ' + search + ' video nya adalah ' + video
+    # if not search:
+    #     return render_template('index.html')
+    # if not video:
+    #     # return 'hasil search adalah ' + search
+    #     return render_template('index.html', search=search)
+
+    return render_template('index.html', search=search, video=video)
+    # return 'hasil search adalah ' + search + ' video nya adalah ' + video
 
 @app.route('/setting')
 def show_setting():
@@ -22,11 +24,18 @@ def show_profile(username):
 
 @app.route('/blog/<int:blog_id>')
 def show_blog(blog_id):
-    return 'ini adlaah halaman blog %s' % blog_id
+    return 'ini adalah halaman blog %s' % blog_id
 
 @app.route('/login', methods=['GET', 'POST'])
 def show_login():
     if request.method == 'POST':
-        return 'Email adalah : ' + request.form['email']
+        resp = make_response('Email adalah : ' + request.form['email'])
+        resp.set_cookie('email_user', request.form['email'])
+        return resp
 
-    return render_template('login.html')    
+    return render_template('login.html')
+
+@app.route('/getcookie')
+def getCookie():
+    email = request.cookies.get('email_user')
+    return 'Email yang tersimpan di cookie adalah ' + email
